5cbc44bc766a5064fe4b9f5938846936f11a40a5
Nick Cain <45472016+ThePascalC@users.noreply.github.com>
2021-12-02 09:33:08 -0600

    Delete CommerceReservation directory

--- CommerceReservation/src/main/java/com/example/CommerceReservation/repository/ReservationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.CommerceReservation.repository;
-
-import com.example.CommerceReservation.domain.Reservation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-
-}
