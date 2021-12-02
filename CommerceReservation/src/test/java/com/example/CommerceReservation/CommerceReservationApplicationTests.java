6d4fa062b365a810cc7f356751cd775285b86b7d
Nick Cain <45472016+ThePascalC@users.noreply.github.com>
2021-12-02 09:35:49 -0600

    Add files via upload

---
+++ CommerceReservation/src/test/java/com/example/CommerceReservation/CommerceReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CommerceReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
